e888f0437c3ee4b2a836234c00a31c1deec9db77
Caio Azevedo <caioba19@gmail.com>
2018-08-30 18:15:26 -0300

    Exemplo de Passagem por Parametro entre Activitys

---
+++ PassagemParametro/app/src/main/java/com/example/caioalmeida/passagemparametro/Carro.java
@@ -0,0 +1,24 @@
+package com.example.caioalmeida.passagemparametro;
+
+import java.io.Serializable;
+
+public class Carro implements Serializable {
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    private String fabricante;
+    private String modelo;
+}
