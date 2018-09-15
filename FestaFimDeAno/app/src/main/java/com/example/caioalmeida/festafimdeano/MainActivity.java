e26640531addd41a155d83ab493c47c68f5a4fb8
Caio Azevedo <caioba19@gmail.com>
2018-09-15 17:33:00 -0300

    Criação de Elementos de Interface e Navegação entre activities

--- FestaFimDeAno/app/src/main/java/com/example/caioalmeida/festafimdeano/MainActivity.java
+++ FestaFimDeAno/app/src/main/java/com/example/caioalmeida/festafimdeano/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.caioalmeida.festafimdeano;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.textToday = findViewById(R.id.textToday);
+        this.mViewHolder.textDaysLeft = findViewById(R.id.textLeft);
+        this.mViewHolder.buttonConfirm = findViewById(R.id.buttonConfirm);
+
+        this.mViewHolder.buttonConfirm.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, DetailsActivity.class);
+        startActivity(intent);
+    }
+
+    private static class ViewHolder{
+        TextView textToday;
+        TextView textDaysLeft;
+        Button buttonConfirm;
     }
 }
