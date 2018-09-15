cd8c78f6c32fb7520d08234d672e2fbe85003e36
Caio Azevedo <caioba19@gmail.com>
2018-09-14 22:50:32 -0300

    Implementação de Intent implicita

--- GridViewCompartilhamento/app/src/main/java/com/example/caioalmeida/gridviewcompartilhamento/MainActivity.java
+++ GridViewCompartilhamento/app/src/main/java/com/example/caioalmeida/gridviewcompartilhamento/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.caioalmeida.gridviewcompartilhamento;
 
 import android.app.Activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener{
 
     GridView gridView;
     imageAdapter adapterImage;
@@ -31,6 +35,34 @@ public class MainActivity extends Activity {
         adapterImage = new imageAdapter(this, list_imagens);
 
         gridView.setAdapter(adapterImage);
+        gridView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = new Intent();
+        switch (position){
+            case 0:
+                intent = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("geo:46.7170627,-71.2884537"));
+                break;
+            case 1:
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(intent.EXTRA_TEXT, "Texto para ser enviado");
+                intent.setType("text/plain");
+                break;
+            case 2:
+                String number = "77777777";
+                Uri uri = Uri.parse("tel:" + number);
+                intent = new Intent(Intent.ACTION_CALL, uri);
+                break;
+            case 3:
+                intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://www.ucb.br/"));
+                break;
+        }
+
+        startActivity(intent);
     }
 }
 
